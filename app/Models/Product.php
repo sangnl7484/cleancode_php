66922eea5094997106f56c75a42c62e811e510de
luongsangit58 <luongsangit58@gmail.com>
2023-04-20 23:28:30 +0700

    sangnl7484 product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,8 +12,24 @@ class Product extends Model
     use HasFactory;
     use HasSlug;
 
-    protected $fillable = ['title', 'description', 'price', 'image', 'published', 'image_mime', 'image_size', 'created_by', 'updated_by'];
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'image',
+        'published',
+        'image_mime',
+        'image_size',
+        'created_by',
+        'updated_by'
+    ];
 
+    /**
+     * @return SlugOptions
+     */
     public function getSlugOptions() : SlugOptions
     {
         return SlugOptions::create()
@@ -21,6 +37,9 @@ class Product extends Model
             ->saveSlugsTo('slug');
     }
 
+    /**
+     * @return string
+     */
     public function getRouteKeyName()
     {
         return 'slug';
