709c102f9ac42a48cef16412005c7a264a22331c
Sangnl7484 <sangnl7484@co-well.vn>
2023-04-20 14:32:23 +0700

    sangnl7484 feature create helper response API

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'HelperAPI' => App\Helpers\HelperAPI::class
     ])->toArray(),
 
 ];
