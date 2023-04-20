6aa6fac5996ddc63100c3a234eca871f3c20b9a5
luongsangit58 <luongsangit58@gmail.com>
2023-04-20 20:19:00 +0700

    tuyennt7289 auth

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,32 +5,165 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class Customer extends Model
 {
     use HasFactory;
 
+    const LIMIT_SQL = 12;
+
+    /**
+     * @var string[]
+     */
+    const ADDRESS_TYPE = ['shippingAddress', 'billingAddress'];
+
+    /**
+     * @var string
+     */
     protected $primaryKey = 'user_id';
 
+    /**
+     * @var string[]
+     */
     protected $fillable = ['first_name', 'last_name', 'phone', 'status',];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return HasOne
+     */
     private function _getAddresses(): HasOne
     {
         return $this->hasOne(CustomerAddress::class, 'customer_id', 'user_id');
     }
 
+    /**
+     * @return HasOne
+     */
     public function shippingAddress(): HasOne
     {
         return $this->_getAddresses()->where('type', '=', 'shipping');
     }
 
+    /**
+     * @return HasOne
+     */
     public function billingAddress(): HasOne
     {
         return $this->_getAddresses()->where('type', '=', 'billing');
+    }
+
+    /**
+     * Scope a query to sort.
+     *
+     * @param Builder $query
+     * @param Request $request
+     * @return Builder
+     */
+    public function scopeSort(Builder $query, Request $request): Builder
+    {
+        if ($request->has('sort_field') && $request->has('sort_direction')) {
+            $sort = $request->get('sort_field');
+            $directory = $request->get('sort_direction');
+            if (Str::contains($sort, '-')) {
+                $sort = Str::remove('-', $sort);
+                $directory = 'ASC';
+            }
+            $query->orderBy($sort, $directory);
+        }
+        return $query;
+    }
+
+    /**
+     * Scope a query to filter.
+     *
+     * @param Builder $query
+     * @param Request $request
+     * @return Builder
+     */
+    public function scopeFilter(Builder $query, Request $request): Builder
+    {
+        if ($request->has('filter')) {
+            //todo
+        }
+        return $query;
+    }
+
+    /**
+     * Scope a query to search.
+     *
+     * @param Builder $query
+     * @param Request $request
+     * @return Builder
+     */
+    public function scopeSearch(Builder $query, Request $request): Builder
+    {
+        if ($request->has('search') && strlen($request->search) > 0) {
+            $search = $request->search;
+            $query->where(DB::raw("CONCAT(first_name, ' ', last_name)"), 'like', "%{$search}%")
+                ->join('users', 'customers.user_id', '=', 'users.id')
+                ->orWhere('users.email', 'like', "%{$search}%")
+                ->orWhere('customers.phone', 'like', "%{$search}%");
+        }
+        return $query;
+    }
+
+    /**
+     * @param Request $request
+     * @return LengthAwarePaginator
+     */
+    public function getAll(Request $request): LengthAwarePaginator
+    {
+        $limit = $request->has('limit') ?? self::LIMIT_SQL;
+        return $this
+            ->sort($request)
+            ->filter($request)
+            ->search($request)
+            ->paginate($limit);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getCustomerById($id)
+    {
+        return $this::find($id);
+    }
+
+    /**
+     * @param $addressData
+     * @return void
+     */
+    public function updateCustomerAddress($addressData): void
+    {
+        CustomerAddress::updateOrCreate(
+            ['customer_id' => $addressData['customer_id'], 'type' => $addressData['type']],
+            $addressData
+        );
+    }
+
+    /**
+     * @param $id
+     * @return void
+     */
+    public function deleteById($id): void
+    {
+        $customer = $this->getCustomerById($id);
+        if ($customer) {
+            DB::table('customer_addresses')->where('customer_id', $id)->delete();
+            $customer->delete();
+        }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name',
         'email',
@@ -20,23 +25,51 @@ class User extends Authenticatable implements MustVerifyEmail
         'is_admin'
     ];
 
+    /**
+     * @var string[]
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-
+    /**
+     * @var string[]
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function customer()
     {
         return $this->hasOne(Customer::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function orders()
     {
         return $this->hasMany(Order::class, 'created_by');
+    }
+
+    /**
+     * @param $request
+     * @return mixed
+     * @throws ValidationException
+     */
+    public function authenticate($request): mixed
+    {
+        $user = $this::where('email', '=', $request->email)->firstOrFail();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user;
     }
 }
