35ee24a8a780cb9afc1868179e98a89a20b179a0
Sangnl7484 <sangnl7484@co-well.vn>
2023-04-21 17:20:21 +0700

    sangnl7484 report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,121 +2,142 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Customer;
 use App\Models\Order;
+use App\Helpers\HelperAPI;
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
+    public const SUB_DAYS_1D = 1;
+    public const SUB_DAYS_1K = 7;
+    public const SUB_DAYS_2K = 14;
+    public const SUB_DAYS_1M = 30;
+    public const SUB_DAYS_3M = 60;
+    public const SUB_DAYS_6M = 180;
 
-
-    public function index(Request $request)
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function index(Request $request): array
     {
-        $paramDate = $request->get('d');
-        $fromDate = null;
-        if($paramDate == '1d'){
-            $fromDate = Carbon::now()->subDays(1);
-        }elseif($paramDate == '1k'){
-            $fromDate = Carbon::now()->subDays(7);
-        }elseif($paramDate == '2k'){
-            $fromDate = Carbon::now()->subDays(14);
-        }elseif($paramDate == '1m'){
-            $fromDate = Carbon::now()->subDays(30);
-        }elseif($paramDate == '3m'){
-            $fromDate = Carbon::now()->subDays(60);
-        }elseif($paramDate == '6m'){
-            $fromDate = Carbon::now()->subDays(180);
-        }
-        $fromDate = $fromDate ?: Carbon::now()->subDay(30);
-        $days = [];
-        $labels = [];
-        $data = [];
-        $now = Carbon::now();
-        while ($fromDate < $now) {
-            $key = $fromDate->format('Y-m-d');
-            $orderList = Order::query()->whereDate('created_at', $key)->get();
-            $grouped = $orderList->groupBy('created_by');
-            $totalUnPaid = 0;
-            $totalPaid = 0;
-            $totalCancelled = 0;
-            $sumShipped = 0;
-            $totalCompleted = 0;
-            foreach($orderList as $item){
-                 if($item->status == 'unpaid'){
-                    $totalUnPaid++;
-                 }
-                 if($item->status == 'paid'){
-                    $totalPaid++;
-                 }
-                 if($item->status == 'cancelled'){
-                    $totalPaid++;
-                 }
-                 if($item->status == 'shipped'){
-                    $sumShipped++;
-                 }
-                 if($item->status == 'completed'){
-                    $totalCompleted++;
-                 }
+        $fromDate = $this->getFromDate($request->get('d'), self::SUB_DAYS_1M);
+        try {
+            $data = [];
+
+            while ($fromDate < Carbon::now()) {
+                $key = $fromDate->format('Y-m-d');
+                $orderList = Order::query()->whereDate('created_at', $key)->get();
+                $grouped = $orderList->groupBy('created_by');
+                $totalUnPaid = 0;
+                $totalPaid = 0;
+                $totalCancelled = 0;
+                $sumShipped = 0;
+                $totalCompleted = 0;
+                foreach ($orderList as $item) {
+                    switch ($item->status) {
+                        case "unpaid":
+                            $totalUnPaid++;
+                            break;
+                        case "paid":
+                            $totalPaid++;
+                            break;
+                        case "cancelled":
+                            $totalCancelled++;
+                            break;
+                        case "shipped":
+                            $sumShipped++;
+                            break;
+                        case "completed":
+                            $totalCompleted++;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                $data[$key] = [
+                    'count_customer' => $grouped->count(),
+                    'count_order' => $orderList->count(),
+                    'unpaid' => $totalUnPaid,
+                    'paid' => $totalPaid,
+                    'cancelled' => $totalCancelled,
+                    'shipped' => $sumShipped,
+                    'completed' => $totalCompleted,
+                    'rate_completed' => round(($totalCompleted / $orderList->count()) * 100) . '%',
+                ];
+                $fromDate = $fromDate->addDay(self::SUB_DAYS_1D);
             }
-            $data[$key] = [
-              'count_customer' =>  $grouped->count(),
-              'count_order' => $orderList->count(),
-              'unpaid' => $totalUnPaid,
-              'paid' => $totalPaid,
-              'cancelled' => $totalCancelled,
-              'shipped' => $sumShipped,
-              'completed' => $totalCompleted,
-              'rate_completed' => round(($totalCompleted/$orderList->count())*100) . '%',
-            ];
-            $fromDate = $fromDate->addDay(1);
+        } catch (\Throwable $e) {
+            return HelperAPI::responseError($e->getMessage());
         }
 
-        return response($data);
-      
+        return HelperAPI::responseSuccess($data);
     }
 
-    public function OrdersReport(Request $request)
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function OrdersReport(Request $request): array
     {
-        $paramDate = $request->get('d');
-        $fromDate = null;
-        if($paramDate == '1d'){
-            $fromDate = Carbon::now()->subDays(1);
-        }elseif($paramDate == '1k'){
-            $fromDate = Carbon::now()->subDays(7);
-        }elseif($paramDate == '2k'){
-            $fromDate = Carbon::now()->subDays(14);
-        }elseif($paramDate == '1m'){
-            $fromDate = Carbon::now()->subDays(30);
-        }elseif($paramDate == '3m'){
-            $fromDate = Carbon::now()->subDays(60);
-        }elseif($paramDate == '6m'){
-            $fromDate = Carbon::now()->subDays(180);
-        }
-        $fromDate = $fromDate ? $fromDate : Carbon::now()->subDay(7);
-        $days = [];
-        $labels = [];
-        $now = Carbon::now();
-        while ($fromDate < $now) {
-            $key = $fromDate->format('Y-m-d');
-            $labels[] = $key;
-            $count = Order::query()->whereDate('created_at', $key)->count();
-            $fromDate = $fromDate->addDay(1);
-            $days[] = $count;
+        try {
+            $fromDate = $this->getFromDate($request->get('d'), self::SUB_DAYS_1K);
+            $days = [];
+            $labels = [];
+            while ($fromDate < Carbon::now()) {
+                $key = $fromDate->format('Y-m-d');
+                $labels[] = $key;
+                $count = Order::query()->whereDate('created_at', $key)->count();
+                $fromDate = $fromDate->addDay(self::SUB_DAYS_1D);
+                $days[] = $count;
+            }
+            $data = [
+                'labels' => $labels,
+                'datasets' => [[
+                    'label' => 'Orders By Day',
+                    'backgroundColor' => '#f87979',
+                    'data' => $days
+                ]]
+            ];
+        } catch (\Throwable $e) {
+            return HelperAPI::responseError($e->getMessage());
         }
 
-        return [
-            'labels' => $labels,
-            'datasets' => [[
-                'label' => 'Orders By Day',
-                'backgroundColor' => '#f87979',
-                'data' => $days
-            ]]
-        ];
+        return HelperAPI::responseSuccess($data);
     }
 
+    /**
+     * @param $paramDate
+     * @param $defaultDate
+     * @return Carbon
+     */
+    public function getFromDate($paramDate, $defaultDate): Carbon
+    {
+        switch ($paramDate) {
+            case "1d":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_1D);
+                break;
+            case "1k":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_1K);
+                break;
+            case "2k":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_2K);
+                break;
+            case "1m":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_1M);
+                break;
+            case "3m":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_3M);
+                break;
+            case "6m":
+                $fromDate = Carbon::now()->subDays(self::SUB_DAYS_6M);
+                break;
+            default:
+                $fromDate = $defaultDate;
+        }
 
+        return $fromDate;
+    }
 }
